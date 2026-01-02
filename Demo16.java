25a189510fbdae994604dafb2cd4b2040467c829
Ankit Saurabh <ankit.saurabh@nagra.com>
2026-01-02 23:21:23 +0530

    19. Functional Interface & Lambda

--- Demo16.java
+++ Demo16.java
@@ -5,13 +5,18 @@ interface A {
     void show();
 }
 
+@FunctionalInterface
+interface B{
+    void tell(int n);
+}
+
 public class Demo16 {
     public static void main(String[] args) { 
         
         // A obj = new A() {
         //     public void show() {
         // Instead of doing all the above just use "->" : Lambda expression
-        
+
         // Lambda expressions only work with Functional Interfaces.
         A obj = () ->
             System.out.println("In show");
@@ -19,5 +24,12 @@ public class Demo16 {
         // };
 
         obj.show(); 
+
+        // Lambda with parameters
+        B objectB = (int i) ->
+            System.out.println("Printing value in tell : "+i);
+        
+        objectB.tell(67);
+        
     }
 }
