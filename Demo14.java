3d089797dec39bf8451cf8682ab4c2feab66faed
Ankit Saurabh <ankit.saurabh@nagra.com>
2026-01-02 22:12:36 +0530

    17. Inner Class & Anonymous Class

--- Demo14.java
+++ Demo14.java
@@ -17,5 +17,27 @@ public class Demo14 {
 
         A.B obj1 = obj.new B(); // Making object of inner class
         obj1.config();
+
+        // Anonymous class can have only methods which are already there in the main class if you are storing it in a variable. You can add features to it with some helper function though
+        A anonymous = new A(){
+            public void show(){
+                System.out.println("Modified the show in anonymous cls");
+                helper();
+            }
+            public void helper(){
+                System.out.println("Added more features to the show");
+            }
+        };
+
+
+        anonymous.show();
+
+        // This works because we haven't stored it in a 'Type A' variable yet!
+        new A(){
+            public void hey(){
+                System.out.println("Hello Anonymous Class");
+            }
+        }.hey();
+        
     }
 }
