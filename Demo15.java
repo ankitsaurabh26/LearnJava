f5e88439510d2371531608de1940f0ba0aa55741
Ankit Saurabh <ankit.saurabh@nagra.com>
2026-01-02 22:42:17 +0530

    18. Interfaces

---
+++ Demo15.java
@@ -0,0 +1,38 @@
+// // One way of doing the things
+// abstract class Computer{
+//     abstract public void code();
+// }
+
+interface Computer {
+    void code();    
+}
+
+// class Laptop extends Computer{
+class Laptop implements Computer{  // in case of interface we use "implements", sort of similar to extends in method 1
+    public void code(){
+        System.out.println("Coding for you!");
+    }
+}
+
+class Desktop implements Computer{
+    public void code(){
+        System.out.println("Coding for you fast!");
+    }
+}
+
+class Developer{
+    public void devApp(Computer lappy){
+        lappy.code();
+    }
+}
+
+public class Demo15 {
+    public static void main(String[] args) {
+
+        Computer mac = new Laptop();
+        Computer Dt = new Desktop();
+        Developer BamBahadur = new Developer();
+        BamBahadur.devApp(mac);
+        BamBahadur.devApp(Dt);
+    }
+}
