900f94861d4f3cc017881f183ed7d6b13840bb3d
Ankit Saurabh <ankit.saurabh@nagra.com>
2026-01-03 15:34:57 +0530

    26. Map

---
+++ Demo23.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Demo23 {
+    public static void main(String[] args) {
+
+        Map<String, Integer> Students = new HashMap<>(); 
+            Students.put("Navneet", 56);
+            Students.put("Kishan", 76);
+            Students.put("Travis", 11);
+
+        System.out.println(Students);
+    }
+}
