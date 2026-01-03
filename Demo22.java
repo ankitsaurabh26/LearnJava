b5d53512d8465ebf6a6b341ffce0d973fd39648c
Ankit Saurabh <ankit.saurabh@nagra.com>
2026-01-03 15:28:46 +0530

    24. Set - HashSet & TreeSet

---
+++ Demo22.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+// import java.util.TreeSet;
+
+public class Demo22 {
+    public static void main(String[] args) {
+        Set<Integer> nums = new HashSet<Integer>(); // No duplicate elements, no sorted order
+        // Set<Integer> nums = new TreeSet<Integer>(); // For sorted value
+        nums.add(11);
+        nums.add(14);
+        nums.add(29);
+        nums.add(11);
+        nums.add(11);
+        nums.add(43);
+        nums.add(14);
+
+        for (int n:nums){
+            System.out.println(n);
+        }
+
+    }
+}
