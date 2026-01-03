314d94a5a9d4fb8ee31f0c7a4b62e2f3940507ef
Ankit Saurabh <ankit.saurabh@nagra.com>
2026-01-03 16:27:31 +0530

    26. Map

--- Demo23.java
+++ Demo23.java
@@ -4,11 +4,20 @@ import java.util.Map;
 public class Demo23 {
     public static void main(String[] args) {
 
-        Map<String, Integer> Students = new HashMap<>(); 
+        Map<String, Integer> Students = new HashMap<>(); // doesn't follow any order
             Students.put("Navneet", 56);
             Students.put("Kishan", 76);
             Students.put("Travis", 11);
+            Students.put("Rohit", 90);
+            Students.put("Kishan", 16);
 
         System.out.println(Students);
+        System.out.println(Students.get("Kishan"));
+        System.out.println(Students.keySet());
+
+        for(String key : Students.keySet()){
+            System.out.println(key + ":"+Students.get(key));
+        }
+
     }
 }
